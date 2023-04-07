660cfd9b6702d013fe097b0bc79b0971415d5cfa
fareezatariq <fareezatariq@gmail.com>
2023-04-07 16:53:23 +0500

    initial layout added at activity_add_players.xml

--- app/src/main/java/com/example/tictactoe/AddPlayers.java
+++ app/src/main/java/com/example/tictactoe/AddPlayers.java
@@ -3,6 +3,8 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPlayers extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class AddPlayers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
+
+        final EditText playerOne= findViewById(R.id.playerOneName);
+        final EditText playerTwo= findViewById(R.id.playerTwoName);
+        final Button startgameBtn= findViewById(R.id.StartGameBtn);
+
+        startgameBtn.setOnClickListener(
+                new
+        );
     }
 }
